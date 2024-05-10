23f701fb8f9faa646d5a4dff6e366d47cfd1fa5d
loicfrom616 <mawash@mailo.com>
2024-05-10 14:12:06 -0400

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,34 @@
 fetch("travel_recommendation_api.json")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        let places = [];
+
+        for (let i = 0;i < data.beaches.length;i++){
+            places.push(data.beaches[i].name);
+            console.log(places);        
+        }
+        for (let y = 0;y < data.countries.length;y++){
+            places.push(data.countries[y].name);
+            console.log(places);        
+        }
+        for (let x = 0;x < data.temples.length;x++){
+            places.push(data.temples[x].name);
+            console.log(places);        
+        }
+        console.log(data)
+         }
+        )        
     .catch(error => console.log(error));
+
+const searchButton = document.getElementById('searchButton');
+const resetButton = document.getElementById('resetButton');
+
+searchButton.addEventListener('click', (e)=>{
+    searchButton.preventdefault;
+    ;
+    
+})
+
+const searchClick = function () {
+
+};
