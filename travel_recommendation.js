9e8c2b4e1af6761179e3308b07c665b7e7e896d9
loicfrom616 <mawash@mailo.com>
2024-05-13 07:43:44 -0400

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,7 +1,7 @@
-
 function searchDestination () {
     const userInput = document.getElementById('searchDestinationField').value.toLowerCase();
     const resultContainer = document.getElementById('resultContainer');
+    resultContainer.innerHTML = "";
 
     let places = [];
 
@@ -19,27 +19,41 @@ function searchDestination () {
                                               <p>${data.beaches[i].description}</p>`;
 
                 resultContainer.style.right = "10%";
-
             }
-
                 console.log(data);
         break;
         case "temples":
         case "temple" : 
-            for (let y = 0;y < data.temples.length;y++){
-            places.push(data.temples[y].name);}
-            console.log(temples)
+            for (let i = 0;i < data.temples.length;i++){
+            places.push(data.temples[i].name);
+            
+            resultContainer.innerHTML += `<img src="${data.temples[i].imageUrl}"></img><br>
+                                              <h2>${data.temples[i].name}</h2>
+                                              <p>${data.temples[i].description}</p>`;
+
+            resultContainer.style.right = "10%";
+            }
+            console.log(data);
         break;
         case "countries":
         case "country" : 
-            for (let u = 0;u < data.countries.length;u++){
-            places.push(data.countries[u].name);}
+        for (let i = 0;i < data.countries.length;i++){
+            let recommandedCountries = data.countries[i];     
+            for (let y = 0; y < recommandedCountries.cities.length;y++){
+                let recommandedCities = recommandedCountries.cities[y];
+                    console.log(recommandedCities);
+                resultContainer.innerHTML += `<img src="${recommandedCities.imageUrl}"></img><br>
+                <h2>${recommandedCities.name}</h2>
+                <p>${recommandedCities.description}</p>`;        
+                resultContainer.style.right = "10%";
+        }
+        }   ;
         break;
         default:
-            console.log("no match, sorry")       
-        }})        
-    .catch(error => console.log(error));
-
+            console.log("no match, sorry");       
+        }
+    })        
+    .catch(error => console.log(error));    
 }
 
 const searchButton = document.getElementById('searchButton');
@@ -48,4 +62,24 @@ const resetButton = document.getElementById('resetButton');
 searchButton.addEventListener('click', ()=>{
     searchButton.preventdefault;
     searchDestination();    
-})
+});
+
+
+// fetch("travel_recommendation_api.json")
+//     .then(response => response.json())
+//     .then(data => {
+// for (let i = 0;i < data.countries.length;i++){
+//     let recommandedCountries = data.countries[i]     
+//     // console.log(data.countries[i]);
+//     for (let y = 0; y < recommandedCountries.cities.length;y++){
+//         let recommandedCities = recommandedCountries.cities[y];
+//             console.log(recommandedCities);
+//         resultContainer.innerHTML += `<img src="${recommandedCities.imageUrl}"></img><br>
+//         <h2>${recommandedCities.name}</h2>
+//         <p>${recommandedCities.description}</p>`;
+
+// resultContainer.style.right = "10%";
+// }
+//     }
+//     }
+// );
