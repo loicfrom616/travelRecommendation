351167efc15779194aa1e96328ff511ff6f00fdc
loicfrom616 <you@example.com>
2024-05-13 14:52:32 -0400

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,6 @@
+const searchButton = document.getElementById('searchButton');
+const resetButton = document.getElementById('resetButton');
+
 function searchDestination () {
     const userInput = document.getElementById('searchDestinationField').value.toLowerCase();
     const resultContainer = document.getElementById('resultContainer');
@@ -22,7 +25,6 @@ function searchDestination () {
                                               </div>`;
 
                 resultContainer.style.right = "10%";
-
                 resultContainer.style.display = "block";
             }
                 console.log(data);
@@ -32,22 +34,14 @@ function searchDestination () {
             for (let i = 0;i < data.temples.length;i++){
             places.push(data.temples[i].name);
             
-            resultContainer.innerHTML += `<img src="${data.temples[i].imageUrl}"></img><br>
-                                              <h2>${data.temples[i].name}</h2>
-                                              <p>${data.temples[i].description}</p>`;
-
-            resultContainer.style.right = "10%";
-
-
             resultContainer.innerHTML += `<div class="destinationContainer">
-            <img src="${data.beaches[i].imageUrl}"></img><br>
-            <h2>${data.beaches[i].name}</h2>
-            <p>${data.beaches[i].description}</p>
+            <img src="${data.temples[i].imageUrl}"></img><br>
+            <h2>${data.temples[i].name}</h2>
+            <p>${data.temples[i].description}</p>
             <button style="margin:5px;width:100px;background:navy;border-radius:5px;border:solid,white">Book now</button>
             </div>`;
-
+            
 resultContainer.style.right = "10%";
-
 resultContainer.style.display = "block";
 
             }
@@ -60,25 +54,41 @@ resultContainer.style.display = "block";
             for (let y = 0; y < recommandedCountries.cities.length;y++){
                 let recommandedCities = recommandedCountries.cities[y];
                     console.log(recommandedCities);
-                resultContainer.innerHTML += `<img src="${recommandedCities.imageUrl}"></img><br>
-                <h2>${recommandedCities.name}</h2>
-                <p>${recommandedCities.description}</p>`;        
-                resultContainer.style.right = "10%";
+             
+                resultContainer.innerHTML += `<div class="destinationContainer">
+            <img src="${recommandedCities.imageUrl}"></img><br>
+            <h2>${recommandedCities.name}</h2>
+            <p>${recommandedCities.description}</p>
+            <button style="margin:5px;width:100px;background:navy;border-radius:5px;border:solid,white">Book now</button>
+            </div>`;            
+
+resultContainer.style.right = "10%";
+resultContainer.style.display = "block";
         }
         }   ;
         break;
         default:
-            console.log("no match, sorry");       
+            console.log("no match, sorry");  
+            resultContainer.innerHTML += `<div class="noMatchContainer">
+            <h2>No match, sorry</h2>
+            </div>`;      
+            resultContainer.style.right = "10%";
+            resultContainer.style.display = "block";            
         }
     })        
     .catch(error => console.log(error));    
 }
 
-const searchButton = document.getElementById('searchButton');
-const resetButton = document.getElementById('resetButton');
+
 
 searchButton.addEventListener('click', ()=>{
     searchButton.preventdefault;
     searchDestination();    
 });
 
+resetButton.addEventListener('click', ()=>{
+    resetButton.preventdefault;
+    resultContainer.innerHTML = "";
+    resultContainer.style.display = "none";
+
+});
